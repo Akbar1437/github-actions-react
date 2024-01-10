be2fadfea2f433c40510d45222bdb79fd631f661
Akbar Khushnazarov <khusnazarovakbar@gmail.com>
2024-01-10 14:53:17 +0500

    add testing workflow

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    // process `*.tsx` files with `ts-jest`
+  },
+  rootDir: "src",
+  moduleNameMapper: {
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
+    "^@app/(.*)$": "<rootDir>/$1",
+    "\\.(css)$": "identity-obj-proxy",
+  },
+};
